8266f42411cc9babc102c8e158369b5dc4745468
Nicole Cordes <cordes@cps-it.de>
2014-08-04 23:10:34 +0200

    [FEATURE] Add TCA configuration

---
+++ Classes/View/ParentView.php
@@ -0,0 +1,62 @@
+<?php
+namespace CPSIT\BeLinks\View;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Nicole Cordes <cordes@cps-it.de>, CPS-IT GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class ParentView {
+
+	/**
+	 * @return void
+	 */
+	public function addMainModuleItems(array $parameters) {
+		$backendModules = $GLOBALS['TBE_MODULES'];
+		// Unset all configuration items
+		unset($backendModules['_PATHS']);
+		unset($backendModules['_dispatcher']);
+		unset($backendModules['_configuration']);
+		unset($backendModules['_navigationComponents']);
+
+		/** @var \TYPO3\CMS\Backend\Module\ModuleLoader $moduleLoader */
+		$moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Module\\ModuleLoader');
+		$moduleLoader->load($backendModules);
+
+		foreach ($backendModules as $moduleName => $submodules) {
+			$moduleName = $moduleLoader->cleanName($moduleName);
+			if (!empty($moduleName)) {
+				$moduleLabel = $moduleName;
+				if (!empty($GLOBALS['LANG']->moduleLabels['tabs'][$moduleName . '_tab'])) {
+					$moduleLabel = $GLOBALS['LANG']->moduleLabels['tabs'][$moduleName . '_tab'];
+				}
+				$parameters['items'][] = array(
+					$moduleLabel,
+					$moduleName
+				);
+			}
+		}
+	}
+
+}
+
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "be_links".
+ *
+ * Auto generated 04-08-2014 16:34
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Backend links',
+	'description' => 'Add page or web links as backend modules',
+	'category' => 'be',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Nicole Cordes',
+	'author_email' => 'cordes@cps-it.de',
+	'author_company' => 'CPS-IT GmbH',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.7-0.0.0',
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
+
+?>
