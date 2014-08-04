5ce4417ece01ef1d0e46df2ddb4163feba996887
Nicole Cordes <cordes@cps-it.de>
2014-08-04 23:13:00 +0200

    [FEATURE] Add backend integration

---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (TYPO3_MODE === 'BE') {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['extTablesInclusion-PostProcessing']['be_links'] =
+		'EXT:be_links/Classes/Hook/BootstrapHook.php:CPSIT\\BeLinks\\Hook\\BootstrapHook';
+}
+
+?>
