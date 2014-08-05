0180e67303b5901f0becda7f1e963f9c8bc61237
Nicole Cordes <cordes@cps-it.de>
2014-08-05 11:13:22 +0200

    [RELEASE] Release of be_links 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "be_links".
  *
- * Auto generated 04-08-2014 16:34
+ * Auto generated 05-08-2014 11:12
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:0:{}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"6b75";s:20:"class.ext_update.php";s:4:"f737";s:12:"ext_icon.gif";s:4:"3405";s:17:"ext_localconf.php";s:4:"8f52";s:14:"ext_tables.sql";s:4:"463a";s:44:"Classes/Controller/BackendLinkController.php";s:4:"8f20";s:30:"Classes/Hook/BootstrapHook.php";s:4:"a796";s:23:"Classes/Module/conf.php";s:4:"0697";s:24:"Classes/Module/index.php";s:4:"39a7";s:33:"Classes/Utility/ModuleUtility.php";s:4:"0ba2";s:27:"Classes/View/ParentView.php";s:4:"ac7c";s:37:"Configuration/TCA/tx_belinks_link.php";s:4:"e121";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"d37f";s:42:"Resources/Public/Icons/tx_belinks_link.gif";s:4:"3405";}',
 	'suggests' => array(
 	),
 );
