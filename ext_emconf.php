f9dbc812394f00421a7ccd60b015383680cf4847
Nicole Cordes <cordes@cps-it.de>
2016-07-12 18:04:49 +0200

    [TASK] Raise TYPO3 dependency to 7.6.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.3.7-0.0.0',
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => 
 		array (
