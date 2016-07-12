283d5cd8c725e379dcbc6ea909cfa525daca6a9a
Nicole Cordes <cordes@cps-it.de>
2016-07-12 13:09:30 +0200

    [TASK] Initialize global variables as $GLOBALS
    Change-Id: I1218763d4faed0472177fed64acc32bc7ab05595

--- Classes/Module/conf.php
+++ Classes/Module/conf.php
@@ -1,6 +1,6 @@
 <?php
 
-$moduleArray = \CPSIT\BeLinks\Utility\ModuleUtility::getModuleArray(\TYPO3\CMS\Core\Utility\GeneralUtility::_GET('M'));
-$MCONF = \CPSIT\BeLinks\Utility\ModuleUtility::getDefaultModuleConfiguration($moduleArray);
+$GLOBALS['moduleArray'] = \CPSIT\BeLinks\Utility\ModuleUtility::getModuleArray(\TYPO3\CMS\Core\Utility\GeneralUtility::_GET('M'));
+$MCONF = \CPSIT\BeLinks\Utility\ModuleUtility::getDefaultModuleConfiguration($GLOBALS['moduleArray']);
 
 ?>
