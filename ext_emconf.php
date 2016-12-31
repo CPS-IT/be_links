f5c326e59cf2583a3eb01008c022ed0f07bb143b
Nicole Cordes <cordes@cps-it.de>
2016-12-31 15:01:33 +0100

    [RELEASE] Release of be_links 0.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "be_links".
  *
- * Auto generated 24-11-2016 12:23
+ * Auto generated 31-12-2016 15:01
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author' => 'Nicole Cordes',
   'author_email' => 'cordes@cps-it.de',
   'author_company' => 'CPS-IT GmbH',
-  'version' => '0.2.1',
+  'version' => '0.3.0',
   'constraints' => 
   array (
     'depends' => 
@@ -36,6 +36,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"e7ef";s:20:"class.ext_update.php";s:4:"c8d8";s:12:"ext_icon.gif";s:4:"3405";s:17:"ext_localconf.php";s:4:"3619";s:14:"ext_tables.sql";s:4:"c077";s:44:"Classes/Controller/BackendLinkController.php";s:4:"f973";s:30:"Classes/Hook/BootstrapHook.php";s:4:"69d9";s:23:"Classes/Module/conf.php";s:4:"4139";s:24:"Classes/Module/index.php";s:4:"3dd6";s:33:"Classes/Utility/ModuleUtility.php";s:4:"c4db";s:27:"Classes/View/ParentView.php";s:4:"b1d9";s:37:"Configuration/TCA/tx_belinks_link.php";s:4:"b171";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"f2ac";s:42:"Resources/Public/Icons/tx_belinks_link.gif";s:4:"3405";}',
+  '_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"7cfb";s:20:"class.ext_update.php";s:4:"c8d8";s:12:"ext_icon.gif";s:4:"3405";s:17:"ext_localconf.php";s:4:"3619";s:14:"ext_tables.sql";s:4:"c077";s:44:"Classes/Controller/BackendLinkController.php";s:4:"f973";s:30:"Classes/Hook/BootstrapHook.php";s:4:"69d9";s:23:"Classes/Module/conf.php";s:4:"4139";s:24:"Classes/Module/index.php";s:4:"3dd6";s:33:"Classes/Utility/ModuleUtility.php";s:4:"bbdb";s:27:"Classes/View/ParentView.php";s:4:"b1d9";s:37:"Configuration/TCA/tx_belinks_link.php";s:4:"b171";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"6317";s:23:"Documentation/Links.rst";s:4:"d18b";s:26:"Documentation/Settings.yml";s:4:"a406";s:46:"Documentation/Images/Introduction/ListView.png";s:4:"8879";s:48:"Documentation/Images/Introduction/ModuleView.png";s:4:"cc20";s:51:"Documentation/Images/UserManual/NewModuleRecord.png";s:4:"927a";s:36:"Documentation/Introduction/Index.rst";s:4:"be27";s:34:"Documentation/UserManual/Index.rst";s:4:"edbf";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"f2ac";s:42:"Resources/Public/Icons/tx_belinks_link.gif";s:4:"3405";}',
 );
 
