e6eba8c5e5670279f68dedf72908b77a18c25354
seirerman <florian.seirer@tirol.gv.at>
2017-02-22 10:56:14 +0100

    Resolves #9

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'tx_belinks_link'
 #
 CREATE TABLE tx_belinks_link (
-    uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    uid int(11) unsigned NOT NULL auto_increment,
     pid int(11) unsigned DEFAULT '0' NOT NULL,
 
     cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE tx_belinks_link (
 
     title varchar(255) DEFAULT '' NOT NULL,
     type int(11) unsigned DEFAULT '0' NOT NULL,
-    url mediumtext DEFAULT '' NOT NULL,
+    url mediumtext NOT NULL,
     icon blob NOT NULL,
     authentication varchar(10) DEFAULT '' NOT NULL,
     parent varchar(30) DEFAULT '0' NOT NULL,
